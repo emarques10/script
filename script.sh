03972e4cf4491e05a67c3c74bf84bdfc0464e898
Ewerton Marques <ewerton.marques@atos.net>
2024-04-02 08:37:57 -0300

    ok

--- script.sh
+++ script.sh
@@ -9,22 +9,31 @@ separator="---"
 # Output file prefix
 output_prefix="output"
 
+# Flag to indicate if the separator has been found
+separator_found=false
+
 # Counter for parts
 part_counter=1
+
+# Output file name for the first part
+output_file="${output_prefix}_${part_counter}.txt"
 
 # Read the input file line by line
 while IFS= read -r line; do
     # If the line matches the separator pattern
     if [[ "$line" == "$separator" ]]; then
+        # Set the separator flag to true
+        separator_found=true
         # Increment the part counter
         ((part_counter++))
+        # Update the output file name for the next part
+        output_file="${output_prefix}_${part_counter}.txt"
         # Skip writing the separator to the output file
         continue
     fi
 
-    # Output file name for the current part
-    output_file="${output_prefix}_${part_counter}.txt"
-    
-    # Write the line to the current output file
-    echo "$line" >> "$output_file"
+    # If the separator has been found, write the line to the current output file
+    if [ "$separator_found" = true ]; then
+        echo "$line" >> "$output_file"
+    fi
 done < "$input_file"
